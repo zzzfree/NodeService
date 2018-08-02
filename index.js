08d79c2f04656e8ca46966a98ea596d9a736bb8c
zzzfree <free@mail.com>
2018-08-02 22:52:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var app = express();
 
 app.all('*', function (req, res) {
-	    res.status( req.query.status || 200 );
-	    if( req.query.delay ){
-		            setTimeout( function(){
-				                res.send( req.query.res || ( req.method + ' success.' ) );
-				            }, req.query.delay );
-		        }else{
-				        res.send( req.query.res || ( req.method + ' success.' ) ); 
-				    }
+    res.status( req.query.status || 200 );
+    if( req.query.delay ){
+        setTimeout( function(){
+            res.send( req.query.res || ( req.method + ' success.' ) );
+        }, req.query.delay );
+    }else{
+        res.send( req.query.res || ( req.method + ' success.' ) ); 
+    }
 });
 
 var server = app.listen(3000, function () {
-	  var host = server.address().address;
-	  var port = server.address().port;
+  var host = server.address().address;
+  var port = server.address().port;
 
-	  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
